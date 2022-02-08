6a845d1fe18f6112747e8baf43bc3c351f7bac94
Ashique-Desai <desai.ashique@gmail.com>
2022-02-08 20:14:27 +0530

    Copy edited

--- components/SectionOne.js
+++ components/SectionOne.js
@@ -4,7 +4,7 @@ export default function SectionOne() {
     return (
         <section className='text-center sm:pt-16 sm:pb-20 sm:mt-8 pt-8 pb-8 px-4'>
             {/* <div className=''> */}
-                <h1 className='sm:text-[40px] text-3xl sm:mb-8 mb-5 font-bold'>Walk Of 'No Shame'</h1>
+                <h1 className='sm:text-[40px] text-3xl sm:mb-8 mb-5 font-bold'>Silly Who?</h1>
                 <p className='sm:w-[55%] sm:text-xl text-base sm:leading-8 leading-8 ml-auto mr-auto'>
                     We're a female-founded brand bringing all-natural, dermatologically-tested safe products designed
                     for people of all genders with huhas (and no, we will never EVER design lightening, brightening and
