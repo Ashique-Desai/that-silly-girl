8c1f23a429be161551786a9d66c7b6cdab2c9227
Ashique-Desai <desai.ashique@gmail.com>
2022-02-09 17:43:34 +0530

    logo changed 3

--- components/Nav.js
+++ components/Nav.js
@@ -115,7 +115,7 @@ export default function NavExperimental3() {
                 <span className="sr-only">Workflow</span>
                 <img
                   className="h-10 w-auto sm:h-14"
-                  src="/logo-2-raster.svg"
+                  src="/logo-3.svg"
                   alt=""
                 />
               </a>
--- components/SocialResponsibility.js
+++ components/SocialResponsibility.js
@@ -14,7 +14,7 @@ export default function SocialResponsibility() {
             </div>
             <div style={{}} className="flex-1">
                 <div className='text-center lg:text-left lg:pt-0 pt-8 pb-8 lg:px-16 px-4'>
-                    <h1 className='sm:text-[40px] text-3xl sm:mb-8 mb-5 font-bold'>We Care. <br className="lg:hidden" />We Aren't So Silly!</h1>
+                    <h1 className='sm:text-[40px] text-3xl sm:mb-8 mb-5 font-bold'>We Care. <br className="lg:hidden" />We Aren't Just Sassy!</h1>
                     <p className='sm:text-xl text-base sm:leading-8 leading-8 ml-auto mr-auto'>
                         We’ve partnered with Lakshyam—that works towards enhancing the livelihoods of girls and women living in slums of Delhi, through menstrual hygiene education and access to sanitary pads made by rural women entrepreneurs. We pledge to donate sanitary pads to 150 such girls and women every month. We are also donating 40 Soothing Sticks every quarter (to be increased basis adoption).
                         When YOU buy from us, we can continue to help THEM.
