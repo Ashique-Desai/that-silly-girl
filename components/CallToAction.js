935884a07a8a79209c1c3c9345c0430e89ab153d
Ashique-Desai <desai.ashique@gmail.com>
2022-02-10 18:40:25 +0530

    shop link hero section

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -1,10 +1,15 @@
 import Link from 'next/link'
+// import { formatter } from '../utils/helpers'
+// import ProductCard from "./ProductCard"
 
-export default function CallToAction() {
+export default function CallToAction({ product }) {
+    
     return (
         <div className='bottom-left-aligned m-8'>
             <h4 className='text-xl pb-2'>Get 50% Off!</h4>
+            <Link  href={`/products/${'jeans'}`}>
             <button className='bg-sassy-pink color-off-white rounded-full px-4 py-2'>SHOP NOW</button>
+            </Link>
         </div>
     )
 }
--- components/SectionOne.js
+++ components/SectionOne.js
@@ -3,14 +3,10 @@ import React from 'react'
 export default function SectionOne() {
     return (
         <section className='text-center sm:pt-16 sm:pb-20 sm:mt-8 pt-8 pb-8 px-4'>
-            {/* <div className=''> */}
-                <h1 className='sm:text-[40px] text-3xl sm:mb-8 mb-5 font-bold'>Sassy, Who?</h1>
-                <p className='sm:w-[55%] sm:text-xl text-base sm:leading-8 leading-8 ml-auto mr-auto'>
-                   We are a female founded brand who understands the sensibilities of young modern women. We understand the cool and the sassy.
-                    {/* May be, being silly is the cost you pay to choose what feels right to you, not minding it even if someone else finds it silly! 
-                    To be yourself. Even if silly. Our fashion too is a little silly, perhaps because its at the very cutting edge.   */}
-                </p>
-            {/* </div> */}
+            <h1 className='sm:text-[40px] text-3xl sm:mb-8 mb-5 font-bold'>Sassy, Who?</h1>
+            <p className='sm:w-[55%] sm:text-xl text-base sm:leading-8 leading-8 ml-auto mr-auto'>
+                We are a female founded brand who understands the sensibilities of young modern women. We understand the cool and the sassy.
+            </p>
         </section>
     )
 }
