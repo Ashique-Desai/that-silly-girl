c84d520557f43f351799afe7128827fdb1bb7f8e
Ashique-Desai <desai.ashique@gmail.com>
2022-02-10 17:54:31 +0530

    section one copy

--- components/SectionOne.js
+++ components/SectionOne.js
@@ -4,10 +4,9 @@ export default function SectionOne() {
     return (
         <section className='text-center sm:pt-16 sm:pb-20 sm:mt-8 pt-8 pb-8 px-4'>
             {/* <div className=''> */}
-                <h1 className='sm:text-[40px] text-3xl sm:mb-8 mb-5 font-bold'>Sassy, who?</h1>
+                <h1 className='sm:text-[40px] text-3xl sm:mb-8 mb-5 font-bold'>Sassy, Who?</h1>
                 <p className='sm:w-[55%] sm:text-xl text-base sm:leading-8 leading-8 ml-auto mr-auto'>
-                    New Ideas, New Tech or even New Fashion is thought to be silly at first but in time people come around.
-                    We do not wish to wait for those people. We want to be silly, now!
+                   We are a female founded brand who understands the sensibilities of young modern women. We understand the cool and the sassy.
                     {/* May be, being silly is the cost you pay to choose what feels right to you, not minding it even if someone else finds it silly! 
                     To be yourself. Even if silly. Our fashion too is a little silly, perhaps because its at the very cutting edge.   */}
                 </p>
