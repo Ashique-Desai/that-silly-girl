7016ce995ca8ff0bb8d6206e1cd316a9f044bb96
Ashique-Desai <desai.ashique@gmail.com>
2022-02-24 17:25:01 +0530

    Clerk auth back again - app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,62 +6,60 @@ import Layout from '../components/Layout'
 import ShopProvider from '../context/shopContext'
 import { useRouter } from 'next/router'
 import '../styles/globals.css'
-// import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs'
+import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs'
 
-// const publicPages = ['/', '/products/[product]', '/faq', '/jobs', '/privacy', '/returns', '/terms', '/shop', '/shipping', '/story']
+const publicPages = ['/', '/products/[product]', '/faq', '/jobs', '/privacy', '/returns', '/terms', '/shop', '/shipping', '/story']
 // Supplying a theme prop for clerk
-//  const theme = {
-//     general: {
-//       color: "#fb8122",
-//       backgroundColor: "#ffffef",
-//       fontColor: "#1d2228",
-//       labelFontWeight: "300",
-//       borderRadius: "0px",
-//     },
-//     buttons: {
-//       fontColor: "#333",
-//     },
-//  }
+ const theme = {
+    general: {
+      color: "#fb8122",
+      backgroundColor: "#ffffef",
+      fontColor: "#1d2228",
+      labelFontWeight: "300",
+      borderRadius: "0px",
+    },
+    buttons: {
+      fontColor: "#333",
+    },
+ }
 
 
 function MyApp({ Component, pageProps }) {
   // Get the pathname
   const { pathname } = useRouter();
   // Check if the current route matches a public page
-  // const isPublicPage = publicPages.includes(pathname)
+  const isPublicPage = publicPages.includes(pathname)
   const router = useRouter()
 
   return (
-      <ShopProvider>
+    <ClerkProvider theme={theme}>
+      {isPublicPage ? (
+        <ShopProvider>
+          <Layout>
+            <Component {...pageProps} key={router.asPath} />
+          </Layout>
+        </ShopProvider>
+
+      ) : (
+        <>
+          <SignedIn>
+            <ShopProvider>
+              <Layout>
+                <Component {...pageProps} key={router.asPath} />
+              </Layout>
+            </ShopProvider>
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </>
+      )}
+      {/* <ShopProvider>
       <Layout>
         <Component {...pageProps} key={router.asPath} />
       </Layout>
-    </ShopProvider> 
-
-    // <ClerkProvider theme={theme}>
-    //   {isPublicPage ? (
-    //     <ShopProvider>
-    //       <Layout>
-    //         <Component {...pageProps} key={router.asPath} />
-    //       </Layout>
-    //     </ShopProvider>
-
-    //   ) : (
-    //     <>
-    //       <SignedIn>
-    //         <ShopProvider>
-    //           <Layout>
-    //             <Component {...pageProps} key={router.asPath} />
-    //           </Layout>
-    //         </ShopProvider>
-    //       </SignedIn>
-    //       <SignedOut>
-    //         <RedirectToSignIn />
-    //       </SignedOut>
-    //     </>
-    //   )}
-     
-    // </ClerkProvider>
+    </ShopProvider> */}
+    </ClerkProvider>
   )
 }
 
