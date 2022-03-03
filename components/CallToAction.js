5b5989f4e8e644b5374d11828b9efd0cdcaa8388
Ashique-Desai <desai.ashique@gmail.com>
2022-03-03 13:03:44 +0530

    Hero image new, Favicon icon

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -5,7 +5,7 @@ export default function CallToAction({ product }) {
     return (
         <div className='bottom-left-aligned m-8'>
             <h4 className='text-xl pb-2'>Get 50% Off!</h4>
-            <Link  href={`/products/${'jeans'}`}>
+            <Link  href={`/products/${'nuon-blue-cut-out-detail-ribbed-top'}`}>
             <button className='bg-sassy-pink color-off-white rounded-full px-4 py-2'>SHOP NOW</button>
             </Link>
         </div>
--- components/Hero.js
+++ components/Hero.js
@@ -1,33 +1,34 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 import Image from 'next/image'
 import heroLarge from '../images/hero-large.jpeg'
-import heroMobile from'../images/hero-mobile.jpeg'
+import heroMobile from '../images/hero-mobile.jpeg'
+// import heroMobileTrial from '../images/delete-hero-mobile.jpg'
+import heroMobileTrial from '../images/hero-mobile.jpg'
 import CallToAction from './CallToAction'
-import SectionWrapper from "../components/SectionWrapper"
+// import SectionWrapper from "../components/SectionWrapper"
 
 export default function Hero() {
   return (
     <section className='block relative bg-off-white mt-8 px-3 pt-4 pb-8'>
-        <div className='relative  md:hero-container-fullscreen'>
-          <div className='hidden xs:block '>
-          <Image className='relative feature-image-cover border-radius-25'
-          src={heroLarge}
-          alt="hero"
-          layout='fill'
-         
-        />
-          </div>
-        <div className='xs:hidden block'>
+      <div className='md:hidden block'>
         <Image className='relative feature-image-cover border-radius-25'
-          src={heroMobile}
+          src={heroMobileTrial}
           alt="hero"
           layout='responsive'
           priority
         />
-        </div>
-        
-        <CallToAction />
-        </div>
-      </section>
+      </div>
+      <div className='relative  md:hero-container-fullscreen'>
+        <div className='hidden md:block '>
+          <Image className='relative feature-image-cover border-radius-25'
+            src={heroLarge}
+            alt="hero"
+            layout='fill'
+
+          />
+      </div>
+      <CallToAction />
+      </div>
+    </section>
   )
 }
