8c5c5979fb9f52fe6ff1bccaa87605c19d085718
Ashique-Desai <desai.ashique@gmail.com>
2022-03-07 18:28:16 +0530

    Copy change - Shipping

--- components/Shipping.js
+++ components/Shipping.js
@@ -4,7 +4,7 @@ import CTALink from './CTALink'
 export default function Shipping() {
     return (
         <section className='text-center sm:pt-16 sm:pb-20 sm:mt-8 pt-8 pb-8 px-4'>
-            <h1 className='sm:text-[40px] text-3xl sm:mb-8 mb-5 font-bold'>Free Shipping + Same Day Delivery</h1>
+            <h1 className='sm:text-[40px] text-3xl sm:mb-8 mb-5 font-bold'>Free Shipping + Next Day Delivery</h1>
             <p className='sm:w-[55%] sm:text-xl text-base sm:leading-8 leading-8 ml-auto mr-auto'>
               We believe in delighting you not just with fantastic fashion products but also world class 
               delivery and fullfilment times! We are not Amazon by any stretch but we are not half as bad :)
