6b54ad34d9cc3a2c7e000003e69b66ebb296e72a
Ashique-Desai <desai.ashique@gmail.com>
2022-03-10 12:47:58 +0530

    Copy Change SectionOne

--- components/SectionOne.js
+++ components/SectionOne.js
@@ -6,7 +6,7 @@ export default function SectionOne() {
         <section className='text-center sm:pt-16 sm:pb-20 sm:mt-8 pt-8 pb-8 px-4'>
             <h1 className='sm:text-[40px] text-3xl sm:mb-8 mb-5 font-bold'>Sassy, Who?</h1>
             <p className='sm:w-[55%] sm:text-xl text-base sm:leading-8 leading-8 ml-auto mr-auto'>
-                We are a female founded brand who understands the sensibilities of modern young women. We understand the cool and the sassy like no other brand.
+                We are a female founded fashion brand who understands the sensibilities of modern young women. We understand the cool and the sassy like no other brand.
                 What say?
             </p>
             <div className='pt-4'>
